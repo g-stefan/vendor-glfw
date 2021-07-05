ae4b445a18d6f954d7e0888b85b6de26d2f5b787
Grigore Stefan <g_stefan@yahoo.com>
2021-07-05 14:26:28 +0300

    update

---
+++ build/msvc.install.sub.cmd
@@ -0,0 +1,17 @@
+@echo off
+rem Public domain
+rem http://unlicense.org/
+rem Created by Grigore Stefan <g_stefan@yahoo.com>
+
+rem --- make
+
+cmd.exe /C build\msvc.make.cmd
+if errorlevel 1 exit 1
+
+rem --- dev
+
+if not exist "%INSTALL_PATH_DEV%\" mkdir "%INSTALL_PATH_DEV%"
+if not exist "%INSTALL_PATH_DEV%\include" mkdir "%INSTALL_PATH_DEV%\include"
+if not exist "%INSTALL_PATH_DEV%\lib" mkdir "%INSTALL_PATH_DEV%\lib"
+xcopy /Y /S /E "temp\glfw\include\" "%INSTALL_PATH_DEV%\include\"
+copy /Y /B "temp\glfw\lib\*.lib" "%INSTALL_PATH_DEV%\lib\*"
